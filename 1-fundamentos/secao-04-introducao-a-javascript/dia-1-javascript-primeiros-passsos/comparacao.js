fc6eb6ba48bf553f13024c1fd94eb2950b8bd222
Lucas X <lucasmxisto@outlook.com>
2022-10-20 18:22:14 -0300

    iniciado o programa de comparacao com if pt 2

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/comparacao.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/comparacao.js
@@ -1,9 +1,19 @@
 const a = 279;
 const b = 45;
+const c = 500;
 
-if( a > b){
-  console.log(a + " é maior que " + b);
-}else{
-  console.log(b + " é maior que " + a);
+
+// if( a > b){
+//   console.log(a + " é maior que " + b);
+// }else{
+//   console.log(b + " é maior que " + a);
+// }
+
+
+if(a > b && a > c){
+  console.log(a + ' é o maior número');
+}else if(b > a && b > c){
+  console.log(b + ' é o maior número');
+}else if(c > a  && c > b){
+  console.log(c + ' é o maior número');
 }
-
