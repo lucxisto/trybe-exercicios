8757887e4595f8bd596723724c75fc4c5579aadd
Lucas X <lucasmxisto@outlook.com>
2022-10-20 15:34:11 -0300

    primeiro exercicio JS

---
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/scripts.js
@@ -0,0 +1,13 @@
+const myName = 'Lucas';
+const birthCity = 'Barra do Piraí';
+let birthYear = 1995;
+
+console.log(myName, birthCity, birthYear)
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Mendes';
+
+console.log(birthCity);
