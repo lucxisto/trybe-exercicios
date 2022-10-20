e4cf11a46cb01abe77a5e71335458807230e813d
Lucas X <lucasmxisto@outlook.com>
2022-10-20 18:18:42 -0300

    iniciado o programa de comparacao com if

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/comparacao.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/comparacao.js
@@ -2,7 +2,8 @@ const a = 279;
 const b = 45;
 
 if( a > b){
-  console.log(a + "é maior que " + b);
+  console.log(a + " é maior que " + b);
 }else{
-  console.log(b + "é maior que " + a);
+  console.log(b + " é maior que " + a);
 }
+
