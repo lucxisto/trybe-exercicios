2a0c1ac7edce92a6b5e0a91bd147e623c5b84007
Lucas X <lucasmxisto@outlook.com>
2022-10-20 18:29:33 -0300

    programa de comparacao com if pt4

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/comparacao.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passsos/comparacao.js
@@ -33,8 +33,8 @@ const c = 90;
 
 //Parte 4
 
-if (a > 0 && b > 0 && c > 0 && a + b + c == 180){
-  console.log(true);
-}else{
-  console.log('Erro! esse não é um triangulo')
-}
+// if (a > 0 && b > 0 && c > 0 && a + b + c == 180){
+//   console.log(true);
+// }else{
+//   console.log('Erro! esse não é um triangulo')
+// }
