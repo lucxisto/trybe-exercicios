5d5082162e2352e44a2a22d02873948828bee666
Lucas X <lucasmxisto@outlook.com>
2022-10-21 16:02:12 -0300

    exercicio "Lidando com Arrays" parte 5

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/lidando-com-array.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-2-javascript-array-e-loop-for/lidando-com-array.js
@@ -1,14 +1,24 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
-for(let i = 0; i < numbers.length; i++){
-  soma += numbers[i];
-}
-media = soma/(numbers.length-1)
 
-if(media > 20){
-  console.log('numero maior que 20');
-}else{
-  console.log('numero menor ou igual a 20');
+let maior = 0;
+// for(let i = 0; i < numbers.length; i++){
+//   soma += numbers[i];
+// }
+// media = soma/(numbers.length-1)
+
+// if(media > 20){
+//   console.log('numero maior que 20');
+// }else{
+//   console.log('numero menor ou igual a 20');
+// }
+// console.log(media);
+
+for(let i = 0; i < numbers.length; i++){
+  if(numbers[i] > maior)
+  {
+    maior = numbers[i];
+  }
 }
-console.log(media);
+console.log(maior);
