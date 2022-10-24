1b98f9ca77f53ba1e0df0941620babe33ddc8f9a
Lucas X <lucasmxisto@outlook.com>
2022-10-24 18:54:14 -0300

    exercicios do dia

---
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/pratica.js
@@ -0,0 +1,17 @@
+// let fatorial = 10;
+// let resultado = 0;
+
+// for(let i = 9; i > 0; i--){
+//   resultado = fatorial * i;
+//   fatorial = resultado;
+// }
+// console.log(resultado);
+
+// let word = 'luquinha';
+// let wordArray = word.split('');
+// let inverseWord = '';
+
+// for(let i = wordArray.length-1; i >= 0; i--){
+//   inverseWord += wordArray[i];
+// }
+// console.log(inverseWord);
