41315ef226875ad0a52f7792e20e16c8a1fde887
Lucas X <lucasmxisto@outlook.com>
2022-10-25 11:36:30 -0300

    exercicios do dia

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/pratica.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-3-javascript-logica-de-programacao-e-algoritmos/pratica.js
@@ -16,6 +16,48 @@
 // }
 // console.log(inverseWord);
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = '';
+// let menorPalavra = '';
 
+// for(let i = 0; i < array.length; i++){
+//   if(array[i].length > maiorPalavra.length){
+//     maiorPalavra = array[i];
+//   }else{
+//     menorPalavra = array[i];
+//   }
+// }
+// console.log('A maior palavra do Array é: ' + maiorPalavra);
+// console.log('A menor palavra do Array é: ' + menorPalavra);
 
+let limiteMinimo = 2;
+let limiteMaximo = 50; 
+let contadorDeDivisores = 0;
+let numerosPrimos = [];
+let maiorPrimo = 0;
+let menorPrimo = 0;
+
+for(let i = limiteMinimo; i <= limiteMaximo; i++ ){
+  for(let divisor = 2; divisor <= i; divisor++){
+    if(i % divisor == 0){
+      contadorDeDivisores++
+    }
+  }
+  if(contadorDeDivisores == 1){
+    numerosPrimos.push(i);
+  }
+  contadorDeDivisores = 0;
+}
+for(let i = 0; i < numerosPrimos.length; i++){
+  if(numerosPrimos[i] > maiorPrimo && numerosPrimos[i] > menorPrimo){
+    maiorPrimo = numerosPrimos[i];
+    if(maiorPrimo < menorPrimo){
+      menorPrimo = maiorPrimo;
+    }
+  }else{
+    menorPrimo = numerosPrimos[i];
+  }
+}
+
+console.log(numerosPrimos);
+console.log(maiorPrimo);
