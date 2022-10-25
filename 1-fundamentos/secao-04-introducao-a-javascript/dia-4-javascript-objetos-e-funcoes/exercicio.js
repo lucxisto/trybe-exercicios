507f85874addd30dc812c065473f231d3fd8512a
Lucas X <lucasmxisto@outlook.com>
2022-10-25 18:39:42 -0300

    exercicios de objetos e funções parte 4

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio.js
@@ -5,7 +5,7 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log('Bem Vinda, ' + info.personagem);
+ console.log('Bem Vinda, ' + info.personagem);
 
 //Parte 2
 info['recorrente'] = 'Sim';
@@ -17,4 +17,7 @@ for (const key in info) {
   console.log(key);
 }
 
-//Parte
+//Parte 4
+for (const key in info) {
+  console.log(info[key]);
+} 
