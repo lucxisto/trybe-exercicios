4aacee6aa1bee5c80298e95953e5a9e088b34bc0
Lucas X <lucasmxisto@outlook.com>
2022-10-26 09:02:56 -0300

    exercicio Bonus - Números Romanos

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-bonus.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-bonus.js
@@ -9,15 +9,25 @@ function translateRomanToNumber(romanN){
     'M' : 1000,
   }
   let romanArray = romanN.split('');
-  let romanValue = [];
+  let romanValueArray = [];
+  let romanValue = 0;
   for(let index = 0; index < romanArray.length; index++){
     for (const key in romanNumber) {
       if(key === romanArray[index]){
-        romanValue.push(romanNumber[key]);
+        romanValueArray.push(romanNumber[key]);
       }
+    }
+  }
+  for(let index = 0; index < romanValueArray.length; index++){
+    if (romanValueArray[index] > romanValueArray[index + 1]) {
+      romanValue += romanValueArray[index];
+    }else if (romanValueArray[index] < romanValueArray[index + 1]){
+      romanValue -= romanValueArray[index];
+    }else{
+      romanValue += romanValueArray[index];
     }
   }
   return romanValue
 }
 
-console.log(translateRomanToNumber('IX'))
+console.log(translateRomanToNumber('MMV'))
