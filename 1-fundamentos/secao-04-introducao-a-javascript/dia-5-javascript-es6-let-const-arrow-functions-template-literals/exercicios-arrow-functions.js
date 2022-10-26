cadf33a1e99d2bc7192da1f26efe5de42eceeda3
Lucas X <lucasmxisto@outlook.com>
2022-10-26 18:30:25 -0300

    Transforme as funções em Arrow Functions P2

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-5-javascript-es6-let-const-arrow-functions-template-literals/exercicios-arrow-functions.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-5-javascript-es6-let-const-arrow-functions-template-literals/exercicios-arrow-functions.js
@@ -4,3 +4,12 @@
 const numeroAleatorio = () => Math.random();
 
 console.log(numeroAleatorio());
+
+// function hello(nome) {
+//   return `Olá, ${nome}!`
+// }
+
+const hello = (nome) => `Olá, ${nome}`;
+
+let nome = 'Ivan';
+console.log(hello(nome));
