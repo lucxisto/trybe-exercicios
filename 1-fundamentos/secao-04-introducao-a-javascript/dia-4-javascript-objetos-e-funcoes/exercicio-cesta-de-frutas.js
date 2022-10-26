fbe944cecd61040f931c0c41a622c4e077f4d956
Lucas X <lucasmxisto@outlook.com>
2022-10-26 09:56:55 -0300

    exercicio Bonus - Cesta de Frutas

--- 1-fundamentos/secao-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-cesta-de-frutas.js
+++ 1-fundamentos/secao-04-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicio-cesta-de-frutas.js
@@ -1,6 +1,7 @@
 function fruitBasket(basket){
   let organizedBasket = {};
   let quantityOfFruit = 0;
+  let outputBasket = 'Sua cesta possui: ';
   for(let indexPrimary = 0; indexPrimary < basket.length; indexPrimary++){
     for(let indexSecondary = 0; indexSecondary < basket.length; indexSecondary++){
       if(basket[indexPrimary] === basket[indexSecondary]){
@@ -10,4 +11,31 @@ function fruitBasket(basket){
     }
     quantityOfFruit = 0;
   }
+  for (const key in organizedBasket) {
+    outputBasket += organizedBasket[key] + ' ' + key + ', ';
+  }
+
+  return outputBasket;
 }
+
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+console.log(fruitBasket(basket));
