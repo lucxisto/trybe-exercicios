9228c4c9fca9e5c343329207fac3d2dc3f3639d5
Lucas X <lucasmxisto@outlook.com>
2022-11-01 10:37:14 -0300

    pratica

--- 1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ 1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -9,3 +9,18 @@ console.log('Primeiro Filho a partir de elementoOndeVoceEsta',whereAreYouElement
 console.log('"Atenção !" a partir de elementoOndeVoceEsta',whereAreYouElement.nextSibling);
 console.log('Terceiro Filho a partir de elementoOndeVoceEsta', whereAreYouElement.nextElementSibling);
 console.log('Terceiro Filho a partir de pai',daddy.childNodes[5]);
+
+const brother = document.createElement('section');
+brother.setAttribute('id', 'irmãoDeElementoOndeVoceEsta');
+const son = document.createElement('img');
+son.setAttribute('id', 'filhoDeElementoOndeVoceEsta');
+const sonOfFirstChild = document.createElement('section');
+sonOfFirstChild.setAttribute('id', 'filhoDoPrimeiroFilho');
+daddy.appendChild(brother);
+whereAreYouElement.appendChild(son);
+whereAreYouElement.firstElementChild.appendChild(sonOfFirstChild);
+
+son.src = './img/imagem.jpg';
+son.className = 'classe';
+
+console.log('Acesso',sonOfFirstChild.parentNode.parentNode.nextElementSibling);
