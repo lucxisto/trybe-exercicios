2864a1dbe4e2011732f6a800cd6a633170c17f9f
Lucas X <lucasmxisto@outlook.com>
2022-11-04 13:18:20 -0300

    Exercícios do Leitor de texto - add changeColor Function

--- 1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ 1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,11 +1,22 @@
 const changeBackground = () => {
-  const main = document.querySelector('main');
+  const main = document.querySelector('#this-main');
   const bgButtons = document.querySelectorAll('.background');
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < bgButtons.length; i++) {
     bgButtons[i].addEventListener('click', (event) => {
-      main.style.backgroundColor = event.target.value;
+      main.style.backgroundColor = event.target.innerText;
+    });
+  }
+}
+
+const changeColor = () => {
+  const main = document.querySelector('#this-main');
+  const colorButtons = document.querySelectorAll('.color');
+  for (let i = 0; i < colorButtons.length; i++) {
+    colorButtons[i].addEventListener('click', (event) => {
+      main.style.color = event.target.innerText;
     });
   }
 }
 
 changeBackground();
+changeColor();
