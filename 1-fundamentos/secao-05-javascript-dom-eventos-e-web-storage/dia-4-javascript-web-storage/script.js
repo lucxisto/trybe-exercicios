6b9e458cb50729f06886154a73532ee79a0d7bd6
Lucas X <lucasmxisto@outlook.com>
2022-11-04 13:29:21 -0300

    Exercícios do Leitor de texto - add changeFontSize and changeLineHeight Function

--- 1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ 1-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -4,6 +4,7 @@ const changeBackground = () => {
   for (let i = 0; i < bgButtons.length; i++) {
     bgButtons[i].addEventListener('click', (event) => {
       main.style.backgroundColor = event.target.innerText;
+      localStorage.setItem('backgroundColor', event.target.innerText);
     });
   }
 }
@@ -14,9 +15,45 @@ const changeColor = () => {
   for (let i = 0; i < colorButtons.length; i++) {
     colorButtons[i].addEventListener('click', (event) => {
       main.style.color = event.target.innerText;
+      localStorage.setItem('color', event.target.innerText);
     });
   }
 }
 
+const changeFontSize = () => {
+  const main = document.querySelector('#this-main');
+  const sizeButtons = document.querySelectorAll('.font-size');
+  for (let i = 0; i < sizeButtons.length; i++) {
+    sizeButtons[i].addEventListener('click', (event) => {
+      main.style.fontSize = event.target.innerText;
+      localStorage.setItem('fontSize', event.target.innerText);
+    });
+  }
+}
+
+const changeLineHeight = () => {
+  const main = document.querySelector('#this-main');
+  const lineHeightButtons = document.querySelectorAll('.line-height');
+  for (let i = 0; i < lineHeightButtons.length; i++) {
+    lineHeightButtons[i].addEventListener('click', (event) => {
+      main.style.lineHeight = event.target.innerText;
+      localStorage.setItem('lineHeight', event.target.innerText);
+    });
+  }
+}
+
+// const changeFontSize = () => {
+//   const main = document.querySelector('#this-main');
+//   const sizeButtons = document.querySelectorAll('.font-size');
+//   for (let i = 0; i < sizeButtons.length; i++) {
+//     sizeButtons[i].addEventListener('click', (event) => {
+//       main.style.fontSize = event.target.innerText;
+//       localStorage.setItem('fontSize', event.target.innerText);
+//     });
+//   }
+// }
+
 changeBackground();
 changeColor();
+changeFontSize();
+changeLineHeight();
