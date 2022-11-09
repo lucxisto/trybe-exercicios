6f79b6e083a1cce87a7d12ee8d6180b3e715a2c9
Lucas X <lucasmxisto@outlook.com>
2022-11-09 10:07:00 -0300

    checando permissões de imagens

--- 1-fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/script.js
+++ 1-fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-e-css-forms/script.js
@@ -1,4 +1,12 @@
 const formSendButton = document.getElementById('send-form');
 formSendButton.addEventListener('click', (event) => {
   event.preventDefault();
+  if(!checkPermission()){
+    window.alert('Para enviar o formulário é necessário autorizar o uso das Imagens');
+  }
 });
+
+const checkPermission = () => {
+  const permissionCheckBox = document.getElementById('permissao-imagens');
+  return permissionCheckBox.checked;
+};
