2c9ef60f28a375d0a2316a3cb69cb72fc385e464
Lucas X <lucasmxisto@outlook.com>
2023-01-25 15:56:43 -0300

    exercicios fixação

--- 2-front-end/secao-03-componentes-com-estado-eventos-formularios/dia-01-componentes-com-estado-eventos/exercicios-fixacao-01/fancy-buttons/src/App.js
+++ 2-front-end/secao-03-componentes-com-estado-eventos-formularios/dia-01-componentes-com-estado-eventos/exercicios-fixacao-01/fancy-buttons/src/App.js
@@ -1,50 +1,17 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      numeroDeCliquesOne: 0,
-      numeroDeCliquesTwo: 0,
-      numeroDeCliquesThree: 0,
-    };
-
-    this.clickingEventOne = this.clickingEventOne.bind(this);
-    this.clickingEventTwo = this.clickingEventTwo.bind(this);
-    this.clickingEventThree = this.clickingEventThree.bind(this);
-  };
-
-  
-
-  clickingEventOne = () => {
-    this.setState((cliquesAnteriores, _props) => ({
-      numeroDeCliquesOne: cliquesAnteriores.numeroDeCliquesOne + 1,
-    }));
-    console.log(`po bicho 1`);
-  };
-  clickingEventTwo = () => {
-    this.setState((cliquesAnteriores, _props) => ({
-      numeroDeCliquesTwo: cliquesAnteriores.numeroDeCliquesTwo + 1,
-    }));
-    console.log(`po bicho2`);
-  };
-  clickingEventThree = () => {
-    this.setState((cliquesAnteriores, _props) => ({
-      numeroDeCliquesThree: cliquesAnteriores.numeroDeCliquesThree + 1,
-    }));
-    console.log(`po bicho3`);
-  };
-
   render() {
-    const { numeroDeCliquesOne, numeroDeCliquesTwo, numeroDeCliquesThree  } = this.state;
     return(
       <div>
-        <button onClick={this.clickingEventOne}>click here {numeroDeCliquesOne}</button>
-        <button onClick={this.clickingEventTwo}>don't click here {numeroDeCliquesTwo}</button>
-        <button onClick={this.clickingEventThree}>fake click here {numeroDeCliquesThree}</button>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
       </div>
     );
   }
--- 2-front-end/secao-03-componentes-com-estado-eventos-formularios/dia-01-componentes-com-estado-eventos/exercicios-fixacao-01/fancy-buttons/src/components/Button/Button.js
+++ 2-front-end/secao-03-componentes-com-estado-eventos-formularios/dia-01-componentes-com-estado-eventos/exercicios-fixacao-01/fancy-buttons/src/components/Button/Button.js
@@ -1,2 +1,33 @@
 import React from 'react';
 
+class Button extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numeroDeCliques: 0,
+    };
+  };
+
+  changButtonColor = (clicksNumber) => clicksNumber % 2 === 0 ? 'green' : 'white';
+
+  clickingEvent = () => {
+    const { numeroDeCliques } = this.state;
+    this.setState((cliquesAnteriores, _props) => ({
+      numeroDeCliques: cliquesAnteriores.numeroDeCliques + 1,
+    }),() => {
+      console.log(`Botão ${this.changButtonColor(numeroDeCliques)}`);
+    });
+  };
+
+  render () {
+    const { numeroDeCliques } = this.state;
+    return(
+      <button onClick={this.clickingEvent}
+          style={ { backgroundColor: this.changButtonColor(numeroDeCliques) } }>
+            click here {numeroDeCliques}
+      </button>
+    )
+  }
+}
+
+export default Button;
